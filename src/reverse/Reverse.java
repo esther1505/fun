a2056d2894c34fdb2b24310179044a90875249b1
antro ranjani <antroranjani2001@gmail.com>
2023-09-09 14:13:10 +0530

    reverse file

---
+++ src/reverse/Reverse.java
@@ -0,0 +1,53 @@
+package reverse;
+
+public class Reverse {
+
+    @SuppressWarnings("empty-statement")
+    public static void main(String[] args) {
+        //Reverse
+//        Scanner in=new Scanner(System.in);
+//        String demo,reverse;
+//        System.out.println("Enter the line: ");
+//        demo=in.nextLine();
+//        int length=demo.length();
+//        for(int i=length-1;i>0;i--)
+//        {
+//            reverse=reverse+demo.charAt(i);
+//        }
+//_________________________________________________________________________________________________________
+
+//         String inp,checkword;
+//         int currentIndex;
+//         System.out.print("Enter the Para: ");
+//         inp=in.nextLine();
+//         System.out.print("Enter the checkword: ");
+//         checkword=in.next();
+//         currentIndex=inp.indexOf(checkword);
+//         int count=0;
+//         while(currentIndex!=-1){
+//             count++;
+//             currentIndex=inp.indexOf(checkword,(currentIndex+1));
+//         }
+//         System.out.print("count: "+count);
+//         
+//         
+//____________________________________________________________________________________________________________________
+        //3D array
+//      
+//        int [][][] ary={{ {3,4,5},{5,6,7},{8,9,10} }};
+//        for (int[][]x:ary){
+//            for(int[]y:x){
+//                for(int z:y)
+//                {
+//                    System.out.print(z+" ");
+//                }
+//                System.out.println();
+//            }
+//        }
+//__________________________________________________________________________________________________________
+ 
+                   
+                   
+    }
+
+}
